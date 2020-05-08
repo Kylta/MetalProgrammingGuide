6479a31a1c5cd6316b150a46a68b936a27a2be1d
cbugnon <christophe.bugnon@musicworldmedia.com>
2020-05-08 11:01:31 +0200

    Create MetalProgrammingGuide project

---
+++ metal-programming-guide/metal-programming-guide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  metal-programming-guide
+//
+//  Created by Christophe Bugnon on 5/8/20.
+//  Copyright © 2020 Christophe Bugnon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+}
+
